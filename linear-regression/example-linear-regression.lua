0941ea905d27034cbcfbd29e055338ae2476378b
Taewoo Lee <twlee@speech.korea.ac.kr>
2015-10-27 12:38:43 +0900

    Translation (English -> Korean)

--- linear-regression/example-linear-regression.lua
+++ linear-regression/example-linear-regression.lua
@@ -1,20 +1,19 @@
 ----------------------------------------------------------------------
--- example-linear-regression.lua
+-- 선형 회귀 예제 (example-linear-regression.lua)
 -- 
--- This script provides a very simple step-by-step example of
--- linear regression, using Torch7's neural network (nn) package,
--- and the optimization package (optim).
---
+-- 이 스크립트는 Torch7의 신경망 패키지(nn)와 
+-- 최적화 패키지(optim)를 사용하여
+-- 선형 회귀의 매우 간단한 단계별 예제를 제공합니다.
 
--- note: to run this script, simply do:
+-- 노트: 이 스크립트 실행하려면, 다음 줄을 실행하십시오:
 -- torch script.lua
 
--- to run the script, and get an interactive shell once it terminates:
+-- 스크립트를 실행하고 쉘과의 상호작용 결과를 얻기 원하면 다음 줄을 실행하십시오:
 -- torch -i script.lua
 
--- we first require the necessary packages.
--- note: optim is a 3rd-party package, and needs to be installed
--- separately. This can be easily done using Torch7's package manager:
+-- 먼저 필요한 패키지들을 require합니다.
+-- 노트: optim은 3rd-party 패키지로 따로 설치되어야 합니다.
+-- 이 설치는 Torch7의 패키지 매니저를 사용하여 다음과 같이 쉽게 할 수 있습니다:
 -- torch-pkg install optim
 
 require 'torch'
@@ -23,36 +22,36 @@ require 'nn'
 
 
 ----------------------------------------------------------------------
--- 1. Create the training data
+-- 1. 훈련 데이터 생성
 
--- In all regression problems, some training data needs to be 
--- provided. In a realistic scenarios, data comes from some database
--- or file system, and needs to be loaded from disk. In that 
--- tutorial, we create the data source as a Lua table.
+-- 모든 회귀 문제들에서, 약간의 훈련 데이터가 필요합니다.
+-- 모든 실제 시나리오에서, 데이터는 파일 시스템의 데이터베이스에서 
+-- 오고, 디스크에서 로드되어야 합니다. 그런 개별 지도에서,
+-- 우리는 데이터 소스를 한 루아 테이블로 만듭니다.
 
--- In general, the data can be stored in arbitrary forms, and using
--- Lua's flexible table data structure is usually a good idea. 
--- Here we store the data as a Torch Tensor (2D Array), where each
--- row represents a training sample, and each column a variable. The
--- first column is the target variable, and the others are the
--- input variables.
+-- 일반적으로, 그 데이터는 임의의 형식으로 저장될 수 있습니다.
+-- 그리고 루아의 유연한 테이블 자료 구조를 사용하는 것은 좋은 생각입니다.
+-- 여기서 우리는 그 데이터를 한 토치 텐서(2차원 배열)로 저장합니다.
+-- 그 텐서의 객 행은 훈련 샘플 하나를 나타냅니다.
+-- 그리고 그 텐서의 각 열은 한 변수를 나타냅니다.
+-- 첫 번째 열은 타겟 변수입니다, 그리고 다른 열들은 입력 변수들입니다.
 
--- The data are from an example in Schaum's Outline:
--- Dominick Salvator and Derrick Reagle
+-- 데이터는 [Schaum's Outline](http://www.mhprofessional.com/category/?cat=3959)의 한 예제입니다:
+-- Dominick Salvator 그리고 Derrick Reagle
 -- Shaum's Outline of Theory and Problems of Statistics and Economics
 -- 2nd edition
 -- McGraw-Hill
 -- 2002
 
--- The data relate the amount of corn produced, given certain amounts
--- of fertilizer and insecticide. See p 157 of the text.
+-- 데이터는 옥수수 생산량을 비료 및 살충제 양과 관련짓습니다.
+-- 그 책의 157 쪽을 보십시오.
 
--- In this example, we want to be able to predict the amount of
--- corn produced, given the amount of fertilizer and insecticide used.
--- In other words: fertilizer & insecticide are our two input variables,
--- and corn is our target value.
+-- 이 예제에서, 우리는 사용된 비료와 살충제 양을 가지고 
+-- 옥수수 생산량을 예측하길 원합니다.
+-- 다른 말로: 비료와 살충제가 우리의 두 입력 변수들이고,
+-- 옥수수가 우리의 타겟 값입니다.
 
---  {corn, fertilizer, insecticide}
+--  {옥수수, 비료, 살충제}
 data = torch.Tensor{
    {40,  6,  4},
    {44, 10,  4},
@@ -68,104 +67,102 @@ data = torch.Tensor{
 
 
 ----------------------------------------------------------------------
--- 2. Define the model (predictor)
+-- 2. 모델 정의 (예측 변수)
 
--- The model will have one layer (called a module), which takes the 
--- 2 inputs (fertilizer and insecticide) and produces the 1 output 
--- (corn).
+-- 모델은 (모듈이라 불리는) 한 층을 가질 것입니다.
+-- 그 층은 두 입력(비료와 살충제)을 받아서 한 출력(옥수수)을 만듭니다.
 
--- Note that the Linear model specified below has 3 parameters:
---   1 for the weight assigned to fertilizer
---   1 for the weight assigned to insecticide
---   1 for the weight assigned to the bias term
+-- 아래 특정된 선형 모델은 세 개의 파라미터를 가짐을 유념하십시오:
+--   하나는 비료에 할당된 가중치
+--   하나는 살충제에 할당된 가중치
+--   하나는 바이어스 항에 할당된 가중치
 
--- In some other model specification schemes, one needs to augment the
--- training data to include a constant value of 1, but this isn't done
--- with the linear model.
+-- 몇몇 다른 모델 특정화 방식에서는, 훈련 데이터가 상숫값 1을 가지도록
+-- 할 수도 있습니다. 그러나 선형 모델에서는 그렇게 하지 않습니다.
 
--- The linear model must be held in a container. A sequential container
--- is appropriate since the outputs of each module become the inputs of 
--- the subsequent module in the model. In this case, there is only one
--- module. In more complex cases, multiple modules can be stacked using
--- the sequential container.
+-- 그 선형 모델은 반드시 컨테이너에 있어야 합니다.
+-- 시퀀셜 컨테이너가 적절합니다, 왜냐하면 각 모듈의 출력들이 
+-- 그 모델의 다음 모듈의 입력이 되기 때문입니다.
+-- 이 경우는, 모듈이 오직 하나만 있습니다.
+-- 더 복잡한 경우들에서, 다수의 모듈들이 
+-- 시퀀셜 컨테이너를 사용하여 쌓일 수 있습니다.
 
--- The modules are all defined in the neural network package, which is
--- named 'nn'.
+-- 그 모듈들은 모두 신경망 패키지 안에 정의되어 있습니다.
+-- 그 패키지의 이름은 'nn'입니다.
 
-model = nn.Sequential()                 -- define the container
+model = nn.Sequential()                 -- 컨테이너를 정의합니다
 ninputs = 2; noutputs = 1
-model:add(nn.Linear(ninputs, noutputs)) -- define the only module
+model:add(nn.Linear(ninputs, noutputs)) -- 그 유일한 모듈을 정의합니다
 
 
 ----------------------------------------------------------------------
--- 3. Define a loss function, to be minimized.
+-- 3. 최소화 되기 위한 손실 함수 정의.
 
--- In that example, we minimize the Mean Square Error (MSE) between
--- the predictions of our linear model and the groundtruth available
--- in the dataset.
+-- 그 예제에서, 우리는 우리의 선형 모델 예측과 그 데이터세트에서 
+-- 사용 가능한 정답 사이의 평균 제곱 오차를 최소화합니다.
 
--- Torch provides many common criterions to train neural networks.
+-- 토치는 신경망을 훈련시키기 위해 흔히 사용되는 많은 판별식들을 제공합니다. 
 
 criterion = nn.MSECriterion()
 
 
 ----------------------------------------------------------------------
--- 4. Train the model
+-- 4. 모델 훈련
 
--- To minimize the loss defined above, using the linear model defined
--- in 'model', we follow a stochastic gradient descent procedure (SGD).
+-- 위에서 정의된 손실을 최소화 하기 위해, '모델'에서 정의된 선형 모델을 
+-- 사용하여, 우리는 통계적 경사 강하(stochastic gradient descent, SGD) 
+-- 절차를 따릅니다. 
 
--- SGD is a good optimization algorithm when the amount of training data
--- is large, and estimating the gradient of the loss function over the 
--- entire training set is too costly.
+-- SGD는 훈련 데이터 양이 많을 때 좋은 최적화 알고리즘입니다.
+-- 그리고 전체 훈련 세트에 걸쳐 그 손실 함수의 기울기를 추정하는 것은
+-- 너무 비용이 많이듭니다.
 
--- Given an arbitrarily complex model, we can retrieve its trainable
--- parameters, and the gradients of our loss function wrt these 
--- parameters by doing so:
+-- 한 임의의 복잡한 모델을 가지고, 우리는 그것의 훈련 가능한 
+-- 파라미터들과 우리의 이 파라미터들에 대한 손실 함수의 기울기들을 
+-- 조사할 수 있습니다. 다음과 같이 함으로써:
 
 x, dl_dx = model:getParameters()
 
--- In the following code, we define a closure, feval, which computes
--- the value of the loss function at a given point x, and the gradient of
--- that function with respect to x. x is the vector of trainable weights,
--- which, in this example, are all the weights of the linear matrix of
--- our model, plus one bias.
+-- 다음 코드에서, 우리는 한 closure, feval을 정의합니다. 
+-- feval은 주어진 시점 x에서의 손실 함수의 값과 x에 대한 그 함수의 
+-- 기울기를 계산합니다. x는 훈련 가능한 가중치들의 벡터입니다.
+-- 그것은, 이 예제에서, 우리 모델의 선형 행렬의 모든 가중치들과
+-- 한 바이어스 입니다.
 
 feval = function(x_new)
-   -- set x to x_new, if differnt
-   -- (in this simple example, x_new will typically always point to x,
-   -- so the copy is really useless)
+   -- 만약 두 변수가 다르면, x를 x_new로 설정합니다. 
+   -- (이 간단한 예제에서, x_new는 보통 항상 x를 가리킵니다,
+   -- 그래서 복사는 필요없습니다)
    if x ~= x_new then
       x:copy(x_new)
    end
 
-   -- select a new training sample
+   -- 새 훈련 예제를 선택합니다
    _nidx_ = (_nidx_ or 0) + 1
    if _nidx_ > (#data)[1] then _nidx_ = 1 end
 
    local sample = data[_nidx_]
-   local target = sample[{ {1} }]      -- this funny looking syntax allows
-   local inputs = sample[{ {2,3} }]    -- slicing of arrays.
+   local target = sample[{ {1} }]      -- 이 우스워 보이는 문법은 
+   local inputs = sample[{ {2,3} }]    -- 배열들의 단면을 쓸 수 있게 합니다.
 
-   -- reset gradients (gradients are always accumulated, to accommodate 
-   -- batch methods)
+   -- 기울기들을 초기화 (기울기들은 항상 누적됩니다, 배치 메소드들을 
+   -- 수용하기 위해)
    dl_dx:zero()
 
-   -- evaluate the loss function and its derivative wrt x, for that sample
+   -- 그 샘플을 위한, 손실 함수의 값과 그 함수의 x에 대한 편미분값을 계산합니다.
    local loss_x = criterion:forward(model:forward(inputs), target)
    model:backward(inputs, criterion:backward(model.output, target))
 
-   -- return loss(x) and dloss/dx
+   -- loss(x)와 dloss/dx를 리턴합니다
    return loss_x, dl_dx
 end
 
--- Given the function above, we can now easily train the model using SGD.
--- For that, we need to define four key parameters:
---   + a learning rate: the size of the step taken at each stochastic 
---     estimate of the gradient
---   + a weight decay, to regularize the solution (L2 regularization)
---   + a momentum term, to average steps over time
---   + a learning rate decay, to let the algorithm converge more precisely
+-- 위에서 주어진 함수를 가지고, 우리는 SGD를 사용하여 모델을 쉽게 훈련시킬 수 있습니다.
+-- 그것을 위해, 네 개의 중요한 파라미터들을 정의할 필요가 있습니다:
+--   + 학습률: 그 기울기의 각 통계적 추정에서 취해지는 스텝의 크기
+--   + 가중치 감소, 그 정답을 레귤러라이즈 하기 위해 (L2 레귤러라이제이션)
+--   + 모멘텀 항, 시간에 걸쳐 스텝들을 평균내기 위해
+--   + 학습률 감소, 그 알고리즘이 더 정확하게 수렴하게 하기 위해
 
 sgd_params = {
    learningRate = 1e-3,
@@ -174,39 +171,39 @@ sgd_params = {
    momentum = 0
 }
 
--- We're now good to go... all we have left to do is run over the dataset
--- for a certain number of iterations, and perform a stochastic update 
--- at each iteration. The number of iterations is found empirically here,
--- but should typically be determinined using cross-validation.
+-- 이제 가봅시다... 이제 우리가 할 일은 데이터세트에서 돌리는 것입니다.
+-- 한 특정한 수의 반복 동안, 그리고 각 반복에서 통계적 갱신을 수행합니다.
+-- 여기서 그 반복의 횟수는 실험적으로 찾아집니다,
+-- 그러나 보통 크로스-밸리데이션(cross-validation)을 사용하여 결정되어야 합니다.
 
--- we cycle 1e4 times over our training data
+-- 우리의 훈련 데이터 전체에 걸쳐 1e4(10,000) 번 돌립니다.
 for i = 1,1e4 do
 
-   -- this variable is used to estimate the average loss
+   -- 이 변수는 평균 손실을 추정하기 위해 사용됩니다
    current_loss = 0
 
-   -- an epoch is a full loop over our training data
+   -- 한 에포크는 우리 훈련 데이터를 완전히 한 번 순회할 때마다 
+   -- 1씩 증가하는 변수입니다.
    for i = 1,(#data)[1] do
 
-      -- optim contains several optimization algorithms. 
-      -- All of these algorithms assume the same parameters:
-      --   + a closure that computes the loss, and its gradient wrt to x, 
-      --     given a point x
-      --   + a point x
-      --   + some parameters, which are algorithm-specific
+      -- optim 몇 개의 최적화 알고리즘을 담고 있습니다.
+      -- 이 모든 알고리즘들은 같은 파라미터들을 가지고 있다고 가정합니다:
+      --   + 손실을 계산하는 closure와 그것의 주어진 point x에 대한 기울기
+      --   + 한 point x
+      --   + 알고리즘에 따라 달라지는 몇몇 파라미터들
       
       _,fs = optim.sgd(feval,x,sgd_params)
 
-      -- Functions in optim all return two things:
-      --   + the new x, found by the optimization method (here SGD)
-      --   + the value of the loss functions at all points that were used by
-      --     the algorithm. SGD only estimates the function once, so
-      --     that list just contains one value.
+      -- optim의 함수들은 모두 두 가지를 리턴합니다:
+      --   + 최적화 메소드로 찾은 (여기서는 SGD) 새로운 x 
+      --   + 그 알고리즘에서 사용되는 모든 point들에서의 손실 함수의 값.
+      --     SGD는 리스트가 오직 하나의 값만 담게 하기 위해 
+      --     오직 그 함수를 한 번만 추정합니다.
 
       current_loss = current_loss + fs[1]
    end
 
-   -- report average error on epoch
+   -- 에포크에서의 평균 에러를 보고합니다
    current_loss = current_loss / (#data)[1]
    print('current loss = ' .. current_loss)
 
@@ -214,16 +211,15 @@ end
 
 
 ----------------------------------------------------------------------
--- 5. Test the trained model.
+-- 5. 훈련된 모델 시험.
 
--- Now that the model is trained, one can test it by evaluating it
--- on new samples.
+-- 모델이 훈련되었으므로, 거기에 새로운 샘플들의 값을 대입해 봄으로써
+-- 그것을 시험할 수 있습니다.
 
--- The text solves the model exactly using matrix techniques and determines
--- that 
+-- 다음 텍스트는 행렬 기법을 사용하여 그 모델을 정확히 풉니다.
 --   corn = 31.98 + 0.65 * fertilizer + 1.11 * insecticides
 
--- We compare our approximate results with the text's results.
+-- 우리는 우리의 근사치 결과들과 텍스트의 결과들을 비교합니다.
 
 text = {40.32, 42.92, 45.33, 48.85, 52.37, 57, 61.82, 69.78, 72.19, 79.42}
 
